b66307a12513d63328dcb83e0740403493165cf8
Ryan Andrews <rpandrews@live.co.uk>
2024-02-18 15:40:22 +0000

    Added image watch functionality to Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,19 @@
-import * as dartSass from 'sass'
-import gulpSass from 'gulp-sass'
-const sass = gulpSass(dartSass)
+import * as dartSass from 'sass';
+import gulpSass from 'gulp-sass';
+const sass = gulpSass(dartSass);
 
-import gulp from 'gulp'
-import cacheBuster from 'gulp-cachebust'
-import runSequence from 'gulp4-run-sequence'
-import { deleteAsync } from 'del'
-import fs from 'fs'
-import concat from 'gulp-concat'
-import terser from 'gulp-terser'
-import cache from 'gulp-cache'
-import imagemin from 'gulp-imagemin'
-import sourcemaps from 'gulp-sourcemaps'
-import cleanCSS from 'gulp-clean-css'
-import autoprefixer from 'gulp-autoprefixer'
+import gulp from 'gulp';
+import cacheBuster from 'gulp-cachebust';
+import runSequence from 'gulp4-run-sequence';
+import { deleteAsync } from 'del';
+import fs from 'fs';
+import concat from 'gulp-concat';
+import terser from 'gulp-terser';
+import cache from 'gulp-cache';
+import imagemin from 'gulp-imagemin';
+import sourcemaps from 'gulp-sourcemaps';
+import cleanCSS from 'gulp-clean-css';
+import autoprefixer from 'gulp-autoprefixer';
 
 const srcDir = './assets'; // Source directory
 const destDir = './dist'; // Destination directory
@@ -67,65 +67,60 @@ gulp.task('cachebust', () => {
 
 // Compile Sass
 gulp.task('sass-frontend', function() {
-  return gulp.src(srcDir +'/scss/style.scss')
+  return gulp.src(srcDir + '/scss/style.scss')
     .pipe(sourcemaps.init())
     .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
     .pipe(autoprefixer())
     .pipe(sourcemaps.write())
     .pipe(concat('style.min.css'))
-    .pipe(gulp.dest(destDir +'/css'));
+    .pipe(gulp.dest(destDir + '/css'));
 });
 
 gulp.task('sass-critical', function() {
-  return gulp.src(srcDir +'/scss/critical.scss')
+  return gulp.src(srcDir + '/scss/critical.scss')
     .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
     .pipe(autoprefixer())
     .pipe(concat('critical.min.css'))
-    .pipe(gulp.dest(destDir +'/css'));
+    .pipe(gulp.dest(destDir + '/css'));
 });
 
 gulp.task('sass-admin', function() {
-  return gulp.src(srcDir +'/scss/admin.scss')
+  return gulp.src(srcDir + '/scss/admin.scss')
     .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
     .pipe(autoprefixer())
     .pipe(concat('admin.min.css'))
-    .pipe(gulp.dest(destDir +'/css'));
+    .pipe(gulp.dest(destDir + '/css'));
 });
 
 gulp.task('sass', gulp.parallel('sass-frontend', 'sass-critical', 'sass-admin'));
 
 // Compile & minify JavaScript
 gulp.task("terser", function() {
-  return gulp.src(srcDir +'/js/*.js')
+  return gulp.src(srcDir + '/js/*.js')
   .pipe(concat('script.min.js'))
   .pipe(terser())
-  .pipe(gulp.dest(destDir +'/js'));
+  .pipe(gulp.dest(destDir + '/js'));
 });
 
 // Compress images
 gulp.task('images', function() {
-  return gulp.src([srcDir +'/images/**/*.+(png|jpg|svg|gif)', "!" + srcDir + "/images/no-compression/*.+(png|jpg|svg|gif)"])
+  return gulp.src(srcDir + '/images/**/*')
   .pipe(cache(imagemin()))
-  .pipe(gulp.dest(destDir +'/images'));
-});
-
-// Move uncompressed images across
-gulp.task('copyUncompressedImages', function(){
-  return gulp.src(srcDir + "/images/no-compression/*.+(png|jpg|svg|gif)")
-  .pipe(gulp.dest(destDir +'/images'));
+  .pipe(gulp.dest(destDir + '/images'));
 });
 
 // Minify CSS
 gulp.task('minify-css',function() {
-  return gulp.src(destDir +'/css/style.min.css')
+  return gulp.src(destDir + '/css/style.min.css')
     .pipe(cleanCSS())
-    .pipe(gulp.dest(destDir +'/css'));
+    .pipe(gulp.dest(destDir + '/css'));
 });
 
 // Watch for changes
 gulp.task('watch', function() {
-  gulp.watch(srcDir +'/scss/**/*.scss', gulp.series('sass'));
-  gulp.watch(srcDir +'/js/*.js', gulp.series('terser'));
+  gulp.watch(srcDir + '/scss/**/*.scss', gulp.series('sass'));
+  gulp.watch(srcDir + '/js/*.js', gulp.series('terser'));
+  gulp.watch(srcDir + '/images/**/*', gulp.series('images'));
 });
 
 // Delete destination folder
@@ -135,5 +130,5 @@ gulp.task('clean:dist', async function() {
 
 // Build assets
 gulp.task('build', function(callback) {
-  runSequence('clean:dist', ['sass', 'terser', 'images', 'copyUncompressedImages'], 'minify-css', 'cachebust', callback);
+  runSequence('clean:dist', ['sass', 'terser', 'images'], 'minify-css', 'cachebust', callback);
 });

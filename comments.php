107ccc083e5f78f5a1c75b77c398599ffbf43638
Ryan Andrews <rpandrews@live.co.uk>
2024-02-18 15:18:02 +0000

    Removed unwanted comments

--- comments.php
+++ comments.php
@@ -7,7 +7,6 @@
 <div id="comments" class="comments-area">
 
 	<?php
-	// You can start editing here -- including this comment!
 	if ( have_comments() ) :
 		?>
 		<h2 class="comments-title">
@@ -46,14 +45,13 @@
 		<?php
 		the_comments_navigation();
 
-		// If comments are closed and there are comments, let's leave a little note, shall we?
 		if ( ! comments_open() ) :
 			?>
 			<p class="no-comments"><?php esc_html_e( 'Comments are closed.', 'whitelabel' ); ?></p>
 			<?php
 		endif;
 
-	endif; // Check for have_comments().
+	endif;
 
 	comment_form();
 	?>

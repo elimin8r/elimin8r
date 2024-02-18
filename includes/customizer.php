2f913c489365651c03c67b0360366c475d143dd0
Ryan Andrews <rpandrews@live.co.uk>
2024-02-18 15:46:20 +0000

    Updated customizer files

--- includes/customizer.php
+++ includes/customizer.php
@@ -1,10 +1,6 @@
 <?php
 
-/**
- * Add postMessage support for site title and description for the Theme Customizer.
- *
- * @param WP_Customize_Manager $wp_customize Theme Customizer object.
- */
+// Add postMessage support for site title and description for the Theme Customizer.
 function whitelabel_customize_register( $wp_customize ) {
 	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
 	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
@@ -29,28 +25,18 @@ function whitelabel_customize_register( $wp_customize ) {
 }
 add_action( 'customize_register', 'whitelabel_customize_register' );
 
-/**
- * Render the site title for the selective refresh partial.
- *
- * @return void
- */
+// Render the site title for the selective refresh partial.
 function whitelabel_customize_partial_blogname() {
 	bloginfo( 'name' );
 }
 
-/**
- * Render the site tagline for the selective refresh partial.
- *
- * @return void
- */
+// Render the site tagline for the selective refresh partial.
 function whitelabel_customize_partial_blogdescription() {
 	bloginfo( 'description' );
 }
 
-/**
- * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
- */
+// Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
 function whitelabel_customize_preview_js() {
-	wp_enqueue_script( 'whitelabel-customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), _S_VERSION, true );
+	wp_enqueue_script( 'whitelabel-customizer', get_template_directory_uri() . '/includes/js/customizer.js', array( 'customize-preview' ), '', true );
 }
 add_action( 'customize_preview_init', 'whitelabel_customize_preview_js' );

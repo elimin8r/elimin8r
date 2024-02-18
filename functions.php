aa415e39451e6410c0850f0b7b815a4a4d1b9625
Ryan Andrews <rpandrews@live.co.uk>
2024-02-18 14:40:55 +0000

    Initial commit

---
+++ functions.php
@@ -0,0 +1,126 @@
+<?php
+
+// Set up theme defaults and registers support for various WordPress features.
+function whitelabel_setup() {
+	// Make theme available for translation.
+	load_theme_textdomain( 'whitelabel', get_template_directory() . '/languages' );
+
+	// Add default posts and comments RSS feed links to head.
+	add_theme_support( 'automatic-feed-links' );
+
+	// Let WordPress manage the document title.
+	add_theme_support( 'title-tag' );
+
+	// Enable support for Post Thumbnails on posts and pages.
+	add_theme_support( 'post-thumbnails' );
+
+	// This theme uses wp_nav_menu() in one location.
+	register_nav_menus(
+		array(
+			'menu-1' => esc_html__( 'Primary', 'whitelabel' ),
+		)
+	);
+
+	// Switch default core markup for search form, comment form, and comments
+	add_theme_support(
+		'html5',
+		array(
+			'search-form',
+			'comment-form',
+			'comment-list',
+			'gallery',
+			'caption',
+			'style',
+			'script',
+		)
+	);
+
+	// Set up the WordPress core custom background feature.
+	add_theme_support(
+		'custom-background',
+		apply_filters(
+			'whitelabel_custom_background_args',
+			array(
+				'default-color' => 'ffffff',
+				'default-image' => '',
+			)
+		)
+	);
+
+	// Add theme support for selective refresh for widgets.
+	add_theme_support( 'customize-selective-refresh-widgets' );
+
+	// Add support for core custom logo.
+	add_theme_support(
+		'custom-logo',
+		array(
+			'height'      => 250,
+			'width'       => 250,
+			'flex-width'  => true,
+			'flex-height' => true,
+		)
+	);
+}
+add_action( 'after_setup_theme', 'whitelabel_setup' );
+
+// Set the content width in pixels, based on the theme's design and stylesheet.
+function whitelabel_content_width() {
+	$GLOBALS['content_width'] = apply_filters( 'whitelabel_content_width', 640 );
+}
+add_action( 'after_setup_theme', 'whitelabel_content_width', 0 );
+
+// Register widget area.
+function whitelabel_widgets_init() {
+	register_sidebar(
+		array(
+			'name'          => esc_html__( 'Sidebar', 'whitelabel' ),
+			'id'            => 'sidebar-1',
+			'description'   => esc_html__( 'Add widgets here.', 'whitelabel' ),
+			'before_widget' => '<section id="%1$s" class="widget %2$s">',
+			'after_widget'  => '</section>',
+			'before_title'  => '<h2 class="widget-title">',
+			'after_title'   => '</h2>',
+		)
+	);
+}
+add_action( 'widgets_init', 'whitelabel_widgets_init' );
+
+// Enqueue scripts and styles.
+function whitelabel_scripts() {
+	$manifest = json_decode( file_get_contents( get_template_directory_uri() . '/dist/manifest.json' ), true );
+	wp_enqueue_style( 'whitelabel-style', get_template_directory_uri() . '/dist/css/' . $manifest['style.min.css'], array(), '' );
+
+	wp_enqueue_script( 'whitelabel-navigation', get_template_directory_uri() . '/dist/css/' . $manifest['script.min.js'], '', true );
+
+	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
+		wp_enqueue_script( 'comment-reply' );
+	}
+}
+add_action( 'wp_enqueue_scripts', 'whitelabel_scripts' );
+
+// Enqueue admin scripts and styles.
+function whitelabel_admin_scripts() {
+	$manifest = json_decode( file_get_contents( get_template_directory_uri() . '/dist/manifest.json' ), true );
+	wp_enqueue_style( 'whitelabel-admin-style', get_template_directory_uri() . '/dist/css/' . $manifest['admin.min.css'], array(), '' );
+}
+add_action( 'admin_enqueue_scripts', 'whitelabel_admin_scripts' );
+
+// Add a pingback url auto-discovery header for single posts, pages, or attachments.
+function whitelabel_pingback_header() {
+	if ( is_singular() && pings_open() ) {
+		printf( '<link rel="pingback" href="%s">', esc_url( get_bloginfo( 'pingback_url' ) ) );
+	}
+}
+add_action( 'wp_head', 'whitelabel_pingback_header' );
+
+// Shim for sites older than 5.2.
+if ( ! function_exists( 'wp_body_open' ) ) :
+	function wp_body_open() {
+		do_action( 'wp_body_open' );
+	}
+endif;
+
+// Include all files in the /includes directory
+foreach ( glob( __DIR__ . '/includes/*.php' ) as $file ) {
+	include $file;
+}
---
+++ gulpfile.js
@@ -0,0 +1,139 @@
+import * as dartSass from 'sass'
+import gulpSass from 'gulp-sass'
+const sass = gulpSass(dartSass)
+
+import gulp from 'gulp'
+import cacheBuster from 'gulp-cachebust'
+import runSequence from 'gulp4-run-sequence'
+import { deleteAsync } from 'del'
+import fs from 'fs'
+import concat from 'gulp-concat'
+import terser from 'gulp-terser'
+import cache from 'gulp-cache'
+import imagemin from 'gulp-imagemin'
+import sourcemaps from 'gulp-sourcemaps'
+import cleanCSS from 'gulp-clean-css'
+import autoprefixer from 'gulp-autoprefixer'
+
+const srcDir = './assets'; // Source directory
+const destDir = './dist'; // Destination directory
+
+// Who you gonna call? Cache buster!
+var cacheBust = new cacheBuster();
+const manifestPath = destDir + '/manifest.json';
+gulp.task('cachebust', () => {
+  return gulp.src([destDir + '/css/*.css', destDir + '/js/*.js']) // Include JS files
+    .pipe(cacheBust.resources())
+    .pipe(gulp.dest(file => {
+      if (file.extname === '.css') {
+        return destDir + '/css';
+      } else if (file.extname === '.js') {
+        return destDir + '/js';
+      }
+    }))
+    .pipe(cacheBust.references())
+    .pipe(gulp.dest(file => {
+      if (file.extname === '.css') {
+        return destDir + '/css';
+      } else if (file.extname === '.js') {
+        return destDir + '/js';
+      }
+    }))
+    .on('end', () => {
+      let manifest = {};
+      ['css', 'js'].forEach(type => {
+        fs.readdir(destDir + '/' + type, (err, files) => {
+          if (err) throw err;
+
+          files.forEach(file => {
+            // Only include .css and .js files
+            const regex = /^[a-z]+\.[a-z]+\.[a-z0-9]+\.(css|js)$/;
+            if (file.match(regex)) {
+              // Remove the hash from the filename
+              let originalFile = file.replace(/\.[a-z0-9]+\.(css|js)$/, '.$1');
+              // Add to the manifest
+              manifest[originalFile] = file;
+            }
+          });
+
+          // Write the manifest to the file
+          fs.writeFile(manifestPath, JSON.stringify(manifest, null, 2), (err) => {
+            if (err) throw err;
+          });
+        });
+      });
+    });
+});
+
+// Compile Sass
+gulp.task('sass-frontend', function() {
+  return gulp.src(srcDir +'/scss/style.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+    .pipe(autoprefixer())
+    .pipe(sourcemaps.write())
+    .pipe(concat('style.min.css'))
+    .pipe(gulp.dest(destDir +'/css'));
+});
+
+gulp.task('sass-critical', function() {
+  return gulp.src(srcDir +'/scss/critical.scss')
+    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+    .pipe(autoprefixer())
+    .pipe(concat('critical.min.css'))
+    .pipe(gulp.dest(destDir +'/css'));
+});
+
+gulp.task('sass-admin', function() {
+  return gulp.src(srcDir +'/scss/admin.scss')
+    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+    .pipe(autoprefixer())
+    .pipe(concat('admin.min.css'))
+    .pipe(gulp.dest(destDir +'/css'));
+});
+
+gulp.task('sass', gulp.parallel('sass-frontend', 'sass-critical', 'sass-admin'));
+
+// Compile & minify JavaScript
+gulp.task("terser", function() {
+  return gulp.src(srcDir +'/js/*.js')
+  .pipe(concat('script.min.js'))
+  .pipe(terser())
+  .pipe(gulp.dest(destDir +'/js'));
+});
+
+// Compress images
+gulp.task('images', function() {
+  return gulp.src([srcDir +'/images/**/*.+(png|jpg|svg|gif)', "!" + srcDir + "/images/no-compression/*.+(png|jpg|svg|gif)"])
+  .pipe(cache(imagemin()))
+  .pipe(gulp.dest(destDir +'/images'));
+});
+
+// Move uncompressed images across
+gulp.task('copyUncompressedImages', function(){
+  return gulp.src(srcDir + "/images/no-compression/*.+(png|jpg|svg|gif)")
+  .pipe(gulp.dest(destDir +'/images'));
+});
+
+// Minify CSS
+gulp.task('minify-css',function() {
+  return gulp.src(destDir +'/css/style.min.css')
+    .pipe(cleanCSS())
+    .pipe(gulp.dest(destDir +'/css'));
+});
+
+// Watch for changes
+gulp.task('watch', function() {
+  gulp.watch(srcDir +'/scss/**/*.scss', gulp.series('sass'));
+  gulp.watch(srcDir +'/js/*.js', gulp.series('terser'));
+});
+
+// Delete destination folder
+gulp.task('clean:dist', async function() {
+  return await deleteAsync(destDir);
+});
+
+// Build assets
+gulp.task('build', function(callback) {
+  runSequence('clean:dist', ['sass', 'terser', 'images', 'copyUncompressedImages'], 'minify-css', 'cachebust', callback);
+});

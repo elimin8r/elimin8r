1d233e754e9007a6a6d870851c8af1f796c8cc5b
Ryan Andrews <rpandrews@live.co.uk>
2024-02-19 18:49:27 +0000

    Update footer background color in footer.php

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 </div><!-- #page -->
 
-<footer id="colophon" class="site-footer" style="<?php echo get_theme_mod( 'footer_color', '#ffffff' ); ?>">
+<footer id="colophon" class="site-footer" style="background: <?php echo get_theme_mod( 'footer_color', '#ffffff' ); ?>">
 	<div class="site-info">
 		<?php echo get_theme_mod( 'footer_text', '' ); ?>
 	</div><!-- .site-info -->

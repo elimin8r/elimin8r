ad2e01b9dc1a1a6839939cb32e91da71d154c564
Ryan Andrews <rpandrews@live.co.uk>
2024-02-20 14:50:31 +0000

    Stopped the site title from displaying if custom logo is set on single posts

--- header.php
+++ header.php
@@ -17,15 +17,13 @@
 		<div class="site-branding">
 			<?php the_custom_logo(); ?>
 
-			<?php if ( is_front_page() || is_home() ) :
-				?>
+			<?php if ( is_front_page() || is_home() ) : ?>
 				<h1 class="site-title <?php echo has_custom_logo() ? 'title-hidden' : ''; ?>"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				<?php
-			else :
-				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php
-			endif;
+			<?php else : ?>
+				<?php if ( ! has_custom_logo( ) ) : ?>
+					<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+				<?php endif; ?>
+			<?php endif;
 
 			$whitelabel_description = get_bloginfo( 'description', 'display' );
 			if ( $whitelabel_description || is_customize_preview() ) :

e69ec50077cde5af26385d59dcecf19320bef9b7
Ryan Andrews <rpandrews@live.co.uk>
2024-02-20 19:42:41 +0000

    Centred the header content

--- header.php
+++ header.php
@@ -14,33 +14,35 @@
 	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'whitelabel' ); ?></a>
 
 	<header id="masthead" class="site-header header-<?php echo get_theme_mod( 'header_position', 'top' ); ?>" style="background: <?php echo get_theme_mod( 'header_color', '#ffffff' ); ?>">
-		<div class="site-branding">
-			<?php the_custom_logo(); ?>
+		<div class="header-content">
+			<div class="site-branding">
+				<?php the_custom_logo(); ?>
 
-			<?php if ( is_front_page() || is_home() ) : ?>
-				<h1 class="site-title <?php echo has_custom_logo() ? 'title-hidden' : ''; ?>"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-			<?php else : ?>
-				<?php if ( ! has_custom_logo( ) ) : ?>
-					<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+				<?php if ( is_front_page() || is_home() ) : ?>
+					<h1 class="site-title <?php echo has_custom_logo() ? 'title-hidden' : ''; ?>"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+				<?php else : ?>
+					<?php if ( ! has_custom_logo( ) ) : ?>
+						<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+					<?php endif; ?>
+				<?php endif;
+
+				$whitelabel_description = get_bloginfo( 'description', 'display' );
+				if ( $whitelabel_description || is_customize_preview() ) :
+					?>
+					<p class="site-description"><?php echo $whitelabel_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
 				<?php endif; ?>
-			<?php endif;
+			</div><!-- .site-branding -->
 
-			$whitelabel_description = get_bloginfo( 'description', 'display' );
-			if ( $whitelabel_description || is_customize_preview() ) :
+			<nav id="site-navigation" class="main-navigation">
+				<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'whitelabel' ); ?></button>
+				<?php
+				wp_nav_menu(
+					array(
+						'theme_location' => 'menu-1',
+						'menu_id'        => 'primary-menu',
+					)
+				);
 				?>
-				<p class="site-description"><?php echo $whitelabel_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
-			<?php endif; ?>
-		</div><!-- .site-branding -->
-
-		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'whitelabel' ); ?></button>
-			<?php
-			wp_nav_menu(
-				array(
-					'theme_location' => 'menu-1',
-					'menu_id'        => 'primary-menu',
-				)
-			);
-			?>
-		</nav><!-- #site-navigation -->
+			</nav><!-- #site-navigation -->
+		</div>
 	</header><!-- #masthead -->

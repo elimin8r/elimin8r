48f6922e78b3516b706c277b4003eb4b44cdfe69
Ryan Andrews <rpandrews@live.co.uk>
2024-02-27 21:41:04 +0000

    Stopped H1 in the header from appearing on static front page

--- header.php
+++ header.php
@@ -18,7 +18,7 @@
 			<div class="site-branding">
 				<?php the_custom_logo(); ?>
 
-				<?php if ( is_front_page() || is_home() ) : ?>
+				<?php if ( is_front_page() && is_home() ) : ?>
 					<h1 class="site-title <?php echo has_custom_logo() ? 'title-hidden' : ''; ?>"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
 				<?php else : ?>
 					<?php if ( ! has_custom_logo( ) ) : ?>

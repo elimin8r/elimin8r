54cf26be12c105c5c45c05b3d1f5f8c975bbc4cc
Ryan Andrews <rpandrews@live.co.uk>
2024-03-03 16:25:28 +0000

    Added ability to disable customizer styles

--- includes/customizer.php
+++ includes/customizer.php
@@ -1,5 +1,9 @@
 <?php
 
+if ( defined( 'WHITELABEL_STYLES' ) && WHITELABEL_STYLES === false ) {
+	return;
+}
+
 // Include all files in the /customizer directory
 foreach ( glob( __DIR__ . '/customizer/*.php' ) as $file ) {
 	include $file;

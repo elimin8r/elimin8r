d03205be6a8a5b1b3a0bb5bb77650abf5924a6f6
Ryan Andrews <rpandrews@live.co.uk>
2024-03-06 19:07:52 +0000

    Fixed sticky post styling on grid layout

--- template-parts/blog-sticky.php
+++ template-parts/blog-sticky.php
@@ -9,34 +9,32 @@ $count = 0;
 ?>
 
 <?php if ( $sticky_posts) :?>
-    <section class="sticky-posts">
-        <?php foreach ( $sticky_posts as $post_id ) : ?>
-            <?php
-                // Only display the first 3 sticky posts
-                $count++;
-                if ( $count > 1 ) {
-                    break;
-                }
+    <?php foreach ( $sticky_posts as $post_id ) : ?>
+        <?php
+            // Only display the first 3 sticky posts
+            $count++;
+            if ( $count > 1 ) {
+                break;
+            }
 
-                $post = get_post( $post_id );
-                setup_postdata( $post );
-            ?>
+            $post = get_post( $post_id );
+            setup_postdata( $post );
+        ?>
 
-            <article class="sticky-post">
-                <a href="<?php the_permalink(); ?>">
-                    <div class="sticky-post-thumbnail">
-                        <?php the_post_thumbnail( 'large' ); ?>
-                    </div>
-                </a>
-                
-                <div class="sticky-post-content">
-                    <a href="<?php the_permalink(); ?>">
-                        <h2><?php the_title(); ?></h2>
-                    </a>
-
-                    <p><?php the_excerpt(); ?></p>
+        <article class="sticky-post">
+            <a href="<?php the_permalink(); ?>">
+                <div class="sticky-post-thumbnail">
+                    <?php the_post_thumbnail( 'large' ); ?>
                 </div>
-            </article>
-        <?php endforeach; ?>
-    </section>
+            </a>
+            
+            <div class="sticky-post-content">
+                <a href="<?php the_permalink(); ?>">
+                    <h2><?php the_title(); ?></h2>
+                </a>
+
+                <p><?php the_excerpt(); ?></p>
+            </div><!-- .sticky-post-content -->
+        </article><!-- .sticky-post -->
+    <?php endforeach; ?>
 <?php endif; ?>

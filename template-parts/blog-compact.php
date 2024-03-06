6fcc2c821d5efee5629c8f02d0a9abe875ad289c
Ryan Andrews <rpandrews@live.co.uk>
2024-03-06 21:51:40 +0000

    Adjusted blog post headings and excerpt styles

--- template-parts/blog-compact.php
+++ template-parts/blog-compact.php
@@ -13,13 +13,6 @@ $current_template = 'blog-compact';
 	<div class="article-content">
 		<header class="entry-header">
 			<?php the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' ); ?>
-	
-			<div class="entry-meta">
-				<?php
-					whitelabel_posted_on();
-					whitelabel_posted_by();
-				?>
-			</div><!-- .entry-meta -->
 		</header><!-- .entry-header -->
 	
 		<div class="entry-content">

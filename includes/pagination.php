5d88515fecc92cd7f868559254508c03cb8e86a2
Ryan Andrews <rpandrews@live.co.uk>
2024-03-09 21:16:04 +0000

    Added pagination styles

--- includes/pagination.php
+++ includes/pagination.php
@@ -9,10 +9,10 @@ function whitelabel_pagination() {
 
     $big = 999999999; // need an unlikely integer
 
-    echo paginate_links( array(
+    echo '<div class="pagination">' . paginate_links( array(
         'base' => str_replace( $big, '%#%', esc_url( get_pagenum_link( $big ) ) ),
         'format' => '?paged=%#%',
         'current' => max( 1, get_query_var('paged') ),
         'total' => $wp_query->max_num_pages
-    ) );
+    ) ) . '</div>';
 }

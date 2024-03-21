c2d8ae236d0d5e440f55fe15ddb8f0c2fd9e8a27
Ryan Andrews <rpandrews@live.co.uk>
2024-03-21 19:54:15 +0000

    Added search input template file

---
+++ searchform.php
@@ -0,0 +1,8 @@
+<form role="search" method="get" class="search-form" action="https://whitelabel.test/">
+    <label>
+        <span class="screen-reader-text">Search for:</span>
+        <input type="search" class="search-field" placeholder="Search…" value="" name="s">
+    </label>
+    
+    <input type="submit" class="search-submit" value="Search">
+</form>

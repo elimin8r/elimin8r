ebb4ab8d55b7faddc6322d1302152821a6196105
Ryan Andrews <rpandrews@live.co.uk>
2024-04-12 07:54:10 +0100

    Refactor customizer default.php to use a class for customizer settings

--- includes/customizer/default.php
+++ includes/customizer/default.php
@@ -1,42 +1,55 @@
 <?php
 
-// Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
-function elimin8r_customize_preview_js() {
-	wp_enqueue_script( 'elimin8r-customizer', get_template_directory_uri() . '/includes/js/customizer.js', array( 'customize-preview' ), '', true );
-}
-add_action( 'customize_preview_init', 'elimin8r_customize_preview_js' );
+/**
+ * Customizer Default
+ * 
+ * @package elimin8r
+ */
 
-// Add postMessage support for site title and description for the Theme Customizer.
-function elimin8r_customize_register( $wp_customize ) {
-	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
-	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
-	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
+class CustomizerDefault {
+	public function __construct()
+	{
+		add_action( 'customize_preview_init', array( $this, 'customize_preview_js' ) );
+		add_action( 'customize_register', array( $this, 'customize_register' ) );
+	}
 
-	if ( isset( $wp_customize->selective_refresh ) ) {
-		$wp_customize->selective_refresh->add_partial(
-			'blogname',
-			array(
-				'selector'        => '.site-title a',
-				'render_callback' => 'elimin8r_customize_partial_blogname',
-			)
-		);
-		$wp_customize->selective_refresh->add_partial(
-			'blogdescription',
-			array(
-				'selector'        => '.site-description',
-				'render_callback' => 'elimin8r_customize_partial_blogdescription',
-			)
-		);
+	public function customize_preview_js()
+	{
+		wp_enqueue_script( 'elimin8r-customizer', get_template_directory_uri() . '/includes/js/customizer.js', array( 'customize-preview' ), '', true );
+	}
+
+	public function customize_register( $wp_customize ) {
+		$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
+		$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
+		$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
+
+		if ( isset( $wp_customize->selective_refresh ) ) {
+			$wp_customize->selective_refresh->add_partial(
+				'blogname',
+				array(
+					'selector'        => '.site-title a',
+					'render_callback' => array( $this, 'customize_partial_blogname' ),
+				)
+			);
+			$wp_customize->selective_refresh->add_partial(
+				'blogdescription',
+				array(
+					'selector'        => '.site-description',
+					'render_callback' => array( $this, 'customize_partial_blogdescription' ),
+				)
+			);
+		}
+	}
+
+	public function customize_partial_blogname()
+	{
+		bloginfo( 'name' );
+	}
+
+	public function customize_partial_blogdescription()
+	{
+		bloginfo( 'description' );
 	}
 }
-add_action( 'customize_register', 'elimin8r_customize_register' );
 
-// Render the site title for the selective refresh partial.
-function elimin8r_customize_partial_blogname() {
-	bloginfo( 'name' );
-}
-
-// Render the site tagline for the selective refresh partial.
-function elimin8r_customize_partial_blogdescription() {
-	bloginfo( 'description' );
-}
+$customizer_default = new CustomizerDefault();

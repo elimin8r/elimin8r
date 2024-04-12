3c00fe4abe6c3c5830aa387f3c257e0f5a4ae911
Ryan Andrews <rpandrews@live.co.uk>
2024-04-12 07:03:34 +0100

    Refactor emojis.php to use a class for disabling emojis

--- includes/settings/emojis.php
+++ includes/settings/emojis.php
@@ -1,36 +1,37 @@
 <?php
 
-// Add Disable Emojis checkbox to Reading settings
-function elimin8r_add_disable_emojis_checkbox() {
-    add_settings_field( 'disable_emojis_checkbox', 'Disable emojis', 'elimin8r_disable_emojis_checkbox_callback', 'reading', 'default' );
-    register_setting( 'reading', 'disable_emojis_checkbox' );
-}
+/**
+ * Disable Emojis
+ *
+ * @package elimin8r
+ */
 
-// Disable Emojis checkbox callback
-function elimin8r_disable_emojis_checkbox_callback() {
-    $emojis = get_option( 'disable_emojis_checkbox' );
-    echo '<input type="checkbox" name="disable_emojis_checkbox" value="1" ' . checked( 1, $emojis, false ) . ' />';
-}
-add_action( 'admin_init', 'elimin8r_add_disable_emojis_checkbox' );
+class EmojiDisabler {
+    public function __construct()
+    {
+        add_action( 'admin_init', array( $this, 'add_disable_emojis_checkbox' ) );
+        if ( get_option( 'disable_emojis_checkbox' ) !== "" ) {
+            add_action( 'init', array( $this, 'disable_emojis' ) );
+        }
+    }
 
-// Disable Emojis
-function elimin8r_disable_emojis() {
-    remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
-    remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
-    remove_action( 'wp_print_styles', 'print_emoji_styles' );
-    remove_action( 'admin_print_styles', 'print_emoji_styles' ); 
-    remove_filter( 'the_content_feed', 'wp_staticize_emoji' );
-    remove_filter( 'comment_text_rss', 'wp_staticize_emoji' ); 
-    remove_filter( 'wp_mail', 'wp_staticize_emoji_for_email' );
-    add_filter( 'tiny_mce_plugins', 'elimin8r_disable_emojis_tinymce' );
-    add_filter( 'wp_resource_hints', 'elimin8r_disable_emojis_remove_dns_prefetch', 10, 2 );
-}
+    // Add Disable Emojis checkbox to Reading settings
+    public function add_disable_emojis_checkbox()
+    {
+        add_settings_field( 'disable_emojis_checkbox', 'Disable emojis', array( $this, 'disable_emojis_checkbox_callback' ), 'reading', 'default' );
+        register_setting( 'reading', 'disable_emojis_checkbox' );
+    }
 
-if ( get_option( 'disable_emojis_checkbox' ) !== "" ) {
-    add_action( 'init', 'elimin8r_disable_emojis' );
+    // Disable Emojis checkbox callback
+    public function disable_emojis_checkbox_callback()
+    {
+        $emojis = get_option( 'disable_emojis_checkbox' );
+        echo '<input type="checkbox" name="disable_emojis_checkbox" value="1" ' . checked( 1, $emojis, false ) . ' />';
+    }
 
     // Filter function used to remove the tinymce emoji plugin
-    function elimin8r_disable_emojis_tinymce( $plugins ) {
+    public function elimin8r_disable_emojis_tinymce( $plugins )
+    {
         if ( is_array( $plugins ) ) {
             return array_diff( $plugins, array( 'wpemoji' ) );
         } else {
@@ -39,7 +40,8 @@ if ( get_option( 'disable_emojis_checkbox' ) !== "" ) {
     }
 
     // Remove emoji CDN hostname from DNS prefetching hints
-    function elimin8r_disable_emojis_remove_dns_prefetch( $urls, $relation_type ) {
+    public function elimin8r_disable_emojis_remove_dns_prefetch( $urls, $relation_type )
+    {
         if ( 'dns-prefetch' == $relation_type ) {
             $emoji_svg_url = apply_filters( 'emoji_svg_url', 'https://s.w.org/images/core/emoji/2/svg/' );
 
@@ -48,4 +50,20 @@ if ( get_option( 'disable_emojis_checkbox' ) !== "" ) {
 
         return $urls;
     }
+
+    // Disable Emojis
+    public function disable_emojis()
+    {
+        remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
+        remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
+        remove_action( 'wp_print_styles', 'print_emoji_styles' );
+        remove_action( 'admin_print_styles', 'print_emoji_styles' ); 
+        remove_filter( 'the_content_feed', 'wp_staticize_emoji' );
+        remove_filter( 'comment_text_rss', 'wp_staticize_emoji' ); 
+        remove_filter( 'wp_mail', 'wp_staticize_emoji_for_email' );
+        add_filter( 'tiny_mce_plugins', array( $this, 'elimin8r_disable_emojis_tinymce' ) );
+        add_filter( 'wp_resource_hints', array( $this, 'elimin8r_disable_emojis_remove_dns_prefetch' ), 10, 2 );
+    }
 }
+
+new EmojiDisabler();

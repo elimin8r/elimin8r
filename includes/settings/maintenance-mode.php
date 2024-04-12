3e6fb23e6c328fa08262485397effa758f9b1900
Ryan Andrews <rpandrews@live.co.uk>
2024-04-12 07:07:31 +0100

    Refactor maintenance-mode.php to use a class for enabling maintenance mode

--- includes/settings/maintenance-mode.php
+++ includes/settings/maintenance-mode.php
@@ -1,32 +1,51 @@
 <?php
 
-function elimin8r_enable_maintenance_mode() {
-    if ( ! get_option( 'maintenance_mode_checkbox' ) === false ) {
-        if ( ! current_user_can( 'edit_themes' ) || ! is_user_logged_in() ) {
-            wp_die( '<h1>Maintenance Mode</h1><p>Sorry, we are currently undergoing maintenance. Please check back later.</p>', 'Maintenance Mode' );
+/**
+ * Maintenance Mode
+ *
+ * @package elimin8r
+ */
+
+class Elimin8r_Maintenance_Mode {
+    public function __construct()
+    {
+        add_action( 'wp', array( $this, 'enable_maintenance_mode' ) );
+        add_action( 'wp_dashboard_setup', array( $this, 'add_maintenance_mode_meta_box' ) );
+        add_action( 'admin_init', array( $this, 'add_maintenance_mode_checkbox' ) );
+    }
+
+    public function enable_maintenance_mode()
+    {
+        if ( ! get_option( 'maintenance_mode_checkbox' ) === false ) {
+            if ( ! current_user_can( 'edit_themes' ) || ! is_user_logged_in() ) {
+                wp_die( '<h1>Maintenance Mode</h1><p>Sorry, we are currently undergoing maintenance. Please check back later.</p>', 'Maintenance Mode' );
+            }
         }
     }
-}
-add_action( 'wp', 'elimin8r_enable_maintenance_mode' );
 
-function elimin8r_add_maintenance_mode_meta_box() {
-    if ( ! get_option( 'maintenance_mode_checkbox' ) === false) {
-        add_meta_box( 'maintenance_mode', 'Maintenance Mode', 'elimin8r_maintenance_mode_meta_box_callback', 'dashboard', 'side', 'high' );
+    public function add_maintenance_mode_meta_box()
+    {
+        if ( ! get_option( 'maintenance_mode_checkbox' ) === false ) {
+            add_meta_box( 'maintenance_mode', 'Maintenance Mode', array( $this, 'maintenance_mode_meta_box_callback' ), 'dashboard', 'side', 'high' );
+        }
+    }
+
+    public function maintenance_mode_meta_box_callback()
+    {
+        echo '<p><span class="dashicons dashicons-hammer"></span> WordPress is currently in maintenance mode.</p>';
+    }
+
+    public function add_maintenance_mode_checkbox()
+    {
+        add_settings_field( 'maintenance_mode_checkbox', 'Enable maintenance mode', array( $this, 'maintenance_mode_checkbox_callback' ), 'reading', 'default' );
+        register_setting( 'reading', 'maintenance_mode_checkbox' );
+    }
+
+    public function maintenance_mode_checkbox_callback()
+    {
+        $maintenance_mode = get_option( 'maintenance_mode_checkbox' );
+        echo '<input type="checkbox" name="maintenance_mode_checkbox" value="1" ' . checked( 1, $maintenance_mode, false ) . ' />';
     }
 }
 
-function elimin8r_maintenance_mode_meta_box_callback() {
-    echo '<p><span class="dashicons dashicons-hammer"></span> WordPress is currently in maintenance mode.</p>';
-}
-add_action( 'wp_dashboard_setup', 'elimin8r_add_maintenance_mode_meta_box' );
-
-function elimin8r_add_maintenance_mode_checkbox() {
-    add_settings_field( 'maintenance_mode_checkbox', 'Enable maintenance mode', 'elimin8r_maintenance_mode_checkbox_callback', 'reading', 'default' );
-    register_setting( 'reading', 'maintenance_mode_checkbox' );
-}
-
-function elimin8r_maintenance_mode_checkbox_callback() {
-    $maintenance_mode = get_option( 'maintenance_mode_checkbox' );
-    echo '<input type="checkbox" name="maintenance_mode_checkbox" value="1" ' . checked( 1, $maintenance_mode, false ) . ' />';
-}
-add_action( 'admin_init', 'elimin8r_add_maintenance_mode_checkbox' );
+new Elimin8r_Maintenance_Mode();

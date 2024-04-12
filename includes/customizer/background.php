5f4132f577776b7fafd2ddc1d7a1a2b0423739ad
Ryan Andrews <rpandrews@live.co.uk>
2024-04-12 07:42:55 +0100

    Refactor background.php to use a class for customizer background settings

--- includes/customizer/background.php
+++ includes/customizer/background.php
@@ -1,31 +1,44 @@
 <?php
 
-function elimin8r_background_customize_register( $wp_customize ) {
-    // Change the section of the background color setting
-    $background_color = $wp_customize->get_control( 'background_color' );
-    if ( ! empty( $background_color ) ) {
-        $background_color->section = 'background_image';
-    }
+/**
+ * Customizer Background
+ * 
+ * @package elimin8r
+ */
 
-	// Add the setting for enabling tint
-	$wp_customize->add_setting( 'background_tint' , array(
-		'default'   => false,
-		'transport' => 'refresh',
-		'sanitize_callback' => 'absint',
-	) );
+class CustomizerBackground {
+	public function __construct() {
+		add_action( 'customize_register', array( $this, 'register_customizer_settings' ) );
+	}
 
-	// Add the control for enabling tint
-	$wp_customize->add_control( new WP_Customize_Control( $wp_customize, 'background_tint', array(
-		'label'      => __( 'Enable Tint', 'elimin8r' ),
-		'section'    => 'background_image',
-		'settings'   => 'background_tint',
-		'type'       => 'checkbox',
-	) ) );
+	public function register_customizer_settings( $wp_customize ) {
+		// Change the section of the background color setting
+		$background_color = $wp_customize->get_control( 'background_color' );
+		if ( ! empty( $background_color ) ) {
+			$background_color->section = 'background_image';
+		}
 
-    // Change the title of the background image section
-    $background_image_section = $wp_customize->get_section( 'background_image' );
-    if ( ! empty( $background_image_section ) ) {
-        $background_image_section->title = 'Background';
-    }
+		// Add the setting for enabling tint
+		$wp_customize->add_setting( 'background_tint' , array(
+			'default'   => false,
+			'transport' => 'refresh',
+			'sanitize_callback' => 'absint',
+		) );
+
+		// Add the control for enabling tint
+		$wp_customize->add_control( new WP_Customize_Control( $wp_customize, 'background_tint', array(
+			'label'      => __( 'Enable Tint', 'elimin8r' ),
+			'section'    => 'background_image',
+			'settings'   => 'background_tint',
+			'type'       => 'checkbox',
+		) ) );
+
+		// Change the title of the background image section
+		$background_image_section = $wp_customize->get_section( 'background_image' );
+		if ( ! empty( $background_image_section ) ) {
+			$background_image_section->title = 'Background';
+		}
+	}
 }
-add_action( 'customize_register', 'elimin8r_background_customize_register' );
+
+$elimin8r_customizer = new CustomizerBackground();

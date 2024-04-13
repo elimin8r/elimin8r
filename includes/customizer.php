7bce34e3008f4ed9147bde4b661242b9ede3e9f0
Ryan Andrews <rpandrews@live.co.uk>
2024-04-13 21:25:10 +0100

    Refactored includes/customizer.php to use namespace

--- includes/customizer.php
+++ includes/customizer.php
@@ -1,5 +1,7 @@
 <?php
 
+namespace Elimin8r\Customizer;
+
 if ( DISABLE_CUSTOMIZER !== true ) {
 	// Include all files in the /customizer directory
 	foreach ( glob( __DIR__ . '/customizer/*.php' ) as $file ) {

51a8e128da81873c1204150c931b59d6581955e3
Ryan Andrews <rpandrews@live.co.uk>
2024-04-13 19:42:40 +0100

    Refactored content-filters.php to use namespace and class structure

--- includes/content-filters.php
+++ includes/content-filters.php
@@ -1,41 +1,58 @@
 <?php
 
-// Add blog-* class to the post_class for each blog template
-function elimin8r_blog_compact_class( $classes ) {
-    global $current_template;
+namespace Elimin8r\ContentFilters;
 
-    if ( $current_template === 'blog-compact' ) {
-        $classes[] = 'blog-compact';
-    } elseif ( $current_template === 'blog-grid' ) {
-        $classes[] = 'blog-grid';
-    } else {
-        $classes[] = 'blog-full';
+/**
+ * Custom content filters
+ *
+ * @package Elimin8r
+ */
+
+class ContentFilters {
+    public function __construct()
+    {
+        add_filter( 'post_class', array( $this, 'blog_compact_class' ) );
+        add_filter('excerpt_more',  array( $this, 'excerpt_more' ) );
+        add_filter( 'excerpt_length',  array( $this, 'excerpt_length' ), 999 );
     }
 
-    return $classes;
-}	
-add_filter( 'post_class', 'elimin8r_blog_compact_class' );
-
-// Add link to end of excerpt
-function elimin8r_excerpt_more( $more ) {
-    return ' <a href="' . get_permalink() . '">' . __('Continue reading', 'elimin8r') . '</a>';
-}
-add_filter('excerpt_more', 'elimin8r_excerpt_more');
-
-// Change excerpt length based on blog template
-function elimin8r_excerpt_length( $length ) {
-    global $current_template;
-
-    if ( $current_template === 'blog-compact' ) {
-        $length = 35;
-    } elseif ( $current_template === 'blog-grid' ) {
-        $length = 0;
-    } elseif ( $current_template === 'blog-full' ) {
-        $length = 55;
-    } else {
-        $length = 55;
+    // Add blog-* class to the post_class for each blog template
+    public function blog_compact_class( $classes ) {
+        global $current_template;
+    
+        if ( $current_template === 'blog-compact' ) {
+            $classes[] = 'blog-compact';
+        } elseif ( $current_template === 'blog-grid' ) {
+            $classes[] = 'blog-grid';
+        } else {
+            $classes[] = 'blog-full';
+        }
+    
+        return $classes;
     }
-
-    return $length;
+    
+    // Add link to end of excerpt
+    public function excerpt_more( $more ) {
+        return ' <a href="' . get_permalink() . '">' . __('Continue reading', 'elimin8r') . '</a>';
+    }
+    
+    // Change excerpt length based on blog template
+    public function excerpt_length( $length ) {
+        global $current_template;
+    
+        if ( $current_template === 'blog-compact' ) {
+            $length = 35;
+        } elseif ( $current_template === 'blog-grid' ) {
+            $length = 0;
+        } elseif ( $current_template === 'blog-full' ) {
+            $length = 55;
+        } else {
+            $length = 55;
+        }
+    
+        return $length;
+    }
 }
-add_filter( 'excerpt_length', 'elimin8r_excerpt_length', 999 );
+
+
+new ContentFilters();

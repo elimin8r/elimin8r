84f766a674992a4660c6ee55d65b33a2c2778d00
Ryan Andrews <rpandrews@live.co.uk>
2024-04-18 06:57:43 +0100

    Toggle header_width control based on header_position setting in CustomizerControls.js

--- includes/customizer/js/customizer-controls.js
+++ includes/customizer/js/customizer-controls.js
@@ -16,12 +16,27 @@
 			wp.customize.control('hamburger_color').container.toggle(device !== 'desktop');
 			wp.customize.control('submenu_color').container.toggle(device === 'desktop');
 			wp.customize.control('submenu_text_color').container.toggle(device === 'desktop');
+
+			// Check header_position and toggle header_width control
+			var headerPosition = wp.customize.control('header_position').setting.get();
+			wp.customize.control('header_width').container.toggle(headerPosition === 'top');
 		}
 
 		checkDevice(); // Check the device on page load
 
 		wp.customize.previewedDevice.bind(function () {
 			checkDevice(); // Check the device when it changes
+		});
+
+		// Only show header_width control when header_position is set to 'top'
+		function checkHeaderPosition() {
+			var headerPosition = wp.customize.control('header_position').setting.get();
+			wp.customize.control('header_width').container.toggle(headerPosition === 'top');
+		}
+
+		// Check header_position when it changes
+		wp.customize.control('header_position').setting.bind(function () {
+			checkHeaderPosition();
 		});
 	});
 }(jQuery));

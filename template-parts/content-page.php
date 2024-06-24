99ce2708928a59578f65f02d44e2c9d33b1af0c5
Ryan Andrews <rpandrews@live.co.uk>
2024-06-24 20:09:49 +0100

    Added setting to hide featured image from single posts and pages

--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -11,7 +11,7 @@
 		$header_position = get_theme_mod( 'header_position', 'top' );
 
 		// If the featured image is not set to full width then display the featured image
-		if ( is_page() && ! get_post_meta( get_the_ID(), '_featured_image_checkbox', true ) || $header_position != 'top') {
+		if ( is_page() && ! get_post_meta( get_the_ID(), '_featured_image_fullwidth', true ) || $header_position != 'top') {
 			Elimin8r\Media\Media::postThumbnail( 'large' );
 		}
 	?>

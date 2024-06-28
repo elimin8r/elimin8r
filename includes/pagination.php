3ce2be6ad8eb842585de6340cad1f7db1ef0de62
Ryan Andrews <rpandrews@live.co.uk>
2024-06-28 21:26:04 +0100

    Update pagination styles for responsive display

--- includes/pagination.php
+++ includes/pagination.php
@@ -23,7 +23,9 @@ class Pagination {
                 'base' => str_replace( $big, '%#%', esc_url( get_pagenum_link( $big ) ) ),
                 'format' => '?paged=%#%',
                 'current' => max( 1, get_query_var('paged') ),
-                'total' => $wp_query->max_num_pages
+                'total' => $wp_query->max_num_pages,
+                'end_size' => 1,
+                'mid_size' => 1,
             ) ) . '</div>';
         }
     }

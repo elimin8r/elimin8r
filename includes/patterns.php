f5343ad13ddbc4f914d3bd49c15e01ec8d7311a8
Ryan Andrews <rpandrews@live.co.uk>
2024-06-29 07:03:43 +0100

    Removed core block patterns

--- includes/patterns.php
+++ includes/patterns.php
@@ -11,14 +11,37 @@ namespace Elimin8r\Patterns;
 class Patterns {
     public function __construct()
     {
+        add_action( 'init', array( $this, 'disableCoreBlockPatterns' ) );
+        add_action( 'init', array( $this, 'removeCoreBlockPatternCategory' ) );
         add_action( 'init', array( $this, 'removeCoreBlockPatterns' ) );
     }
 
-    // Remove core block patterns
-    public function removeCoreBlockPatterns()
+    // Disable core block patterns
+    public function disableCoreBlockPatterns()
     {
         # Note: This does not currently remove the core block patterns from the inserter, which is a known issue -  https://github.com/WordPress/gutenberg/issues/55107
         remove_theme_support( 'core-block-patterns' );
+    }
+
+    // Remove core block patterns
+    function removeCoreBlockPatterns() {
+        $patterns = array(
+			'query-standard-posts',
+			'query-medium-posts',
+			'query-small-posts',
+			'query-grid-posts',
+			'query-large-title-posts',
+			'query-offset-posts'
+        );
+
+        foreach ( $patterns as $pattern ) {
+            unregister_block_pattern( 'core/' . $pattern );
+        }
+    }
+
+    // Remove core block pattern categories
+    public function removeCoreBlockPatternCategory() {
+        unregister_block_pattern_category( 'posts' );
     }
 }
 

9a72f790bef0ef533eb6380773c86d05f1da9d3a
Ryan Andrews <rpandrews@live.co.uk>
2024-07-06 16:57:56 +0100

    Added 'Dont show me again' button

--- includes/notices.php
+++ includes/notices.php
@@ -21,6 +21,7 @@ class Notices {
     function noticeDismissed()
     {
         $this->user_id = get_current_user_id();
+
         if ( isset( $_GET['elimin8r-notice-dismissed'] ) ) {
             add_user_meta( $this->user_id, 'noticeDismissed', 'true', true );
         }
@@ -44,20 +45,20 @@ class Notices {
 
         ?>
         
-        <div class="notice notice-info is-dismissible">
+        <div class="notice notice-info">
             <?php if ( $logo ): ?>
                 <img src="<?php echo esc_url( $logo ); ?>" alt="Elimin8r Logo" style="max-width:200px;height:auto;margin-top:10px;">
             <?php endif; ?>
 
             <h2>Thank you for choosing Elimin8r!</h2>
 
-            <p>We hope you're enjoying Elimin8r. If you are please feel free to buy me a coffee. You contribution will support further development of Elimin8r and help with the late nights spent working on new features, when caffeine is needed.</p>
-
-            <a href="https://buymeacoffee.com/elimin8r" target="_blank" rel="nofollow noreferrer" style="align-items:center;color:#fff;display:flex;background:linear-gradient(135deg,rgb(20,236,224) 0%,rgb(81,112,254) 50%,rgb(236,13,250) 100%);height:40px;width:140px;justify-content:center;text-decoration:none;border-radius:40px;margin:20px 0;">Buy me a coffee</a>
+            <p>We hope you're enjoying Elimin8r. If you are, please feel free to buy me a coffee to help support further development of Elimin8r.</p>
 
             <p>Stay up-to-date with the latest news and theme updates by visiting <a href="https://www.elimin8r.com" target="_blank" rel="nofollow noreferrer">elimin8r.com</a>.</p>
+            
+            <a href="https://buymeacoffee.com/elimin8r" target="_blank" rel="nofollow noreferrer" style="align-items:center;color:#fff;display:flex;background:linear-gradient(135deg,rgb(20,236,224) 0%,rgb(81,112,254) 50%,rgb(236,13,250) 100%);height:40px;width:140px;justify-content:center;text-decoration:none;border-radius:40px;margin:20px 0 0 0;">Buy me a coffee</a>
 
-            <a href="?elimin8r-notice-dismissed">Don't show me again</a>
+            <a href="?elimin8r-notice-dismissed" style="display:block;width:100%;text-align:right;margin:0 0 10px 0;">Don't show me again</a>
         </div>
 
         <?php 

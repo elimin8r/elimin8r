8f05323b4afc6b27ca8c3798743a62368784a1c2
Ryan Andrews <rpandrews@live.co.uk>
2024-07-06 16:23:38 +0100

    Added coffee button to notice

--- includes/notices.php
+++ includes/notices.php
@@ -32,7 +32,11 @@ class Notices {
                 <img src="<?php echo esc_url( $logo ); ?>" alt="Elimin8r Logo" style="max-width:200px;height:auto;margin-top:10px;">
             <?php endif; ?>
 
-            <h2>Thank you for choosing the <span style="color:#ec0dfa;">Elimin8r free WordPress theme!</span></h2>
+            <h2>Thank you for choosing Elimin8r!</h2>
+
+            <p>We hope you're enjoying Elimin8r. If you are please feel free to buy me a coffee. You contribution will support further development of Elimin8r and help with the late nights spent working on new features, when caffeine is needed.</p>
+
+            <a href="https://buymeacoffee.com/elimin8r" target="_blank" rel="nofollow noreferrer" style="align-items:center;color:#fff;display:flex;background:linear-gradient(135deg,rgb(20,236,224) 0%,rgb(81,112,254) 50%,rgb(236,13,250) 100%);height:40px;width:140px;justify-content:center;text-decoration:none;border-radius:40px;margin:20px 0;">Buy me a coffee</a>
 
             <p>Stay up-to-date with the latest news and theme updates by visiting <a href="https://www.elimin8r.com" target="_blank" rel="nofollow noreferrer">elimin8r.com</a>.</p>
         </div>

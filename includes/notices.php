49205537bc125428673b4c3449545f731d6fec9f
Ryan Andrews <rpandrews@live.co.uk>
2024-07-06 16:42:13 +0100

    Added notice dismissal button

--- includes/notices.php
+++ includes/notices.php
@@ -9,14 +9,31 @@ namespace Elimin8r\Notices;
  */
 
 class Notices {
+    public $user_id;
+
     public function __construct()
     {
+        add_action( 'admin_init', array( $this, 'noticeDismissed' ) );
         add_action( 'admin_notices', array( $this, 'addNotice' ) );
+    }
+
+    // Dismiss notice
+    function noticeDismissed()
+    {
+        $this->user_id = get_current_user_id();
+        if ( isset( $_GET['elimin8r-notice-dismissed'] ) ) {
+            add_user_meta( $this->user_id, 'noticeDismissed', 'true', true );
+        }
     }
 
     // Add admin notice
     public function addNotice()
     {
+        // Don't show notice if dismissed
+        if ( get_user_meta( $this->user_id, 'noticeDismissed' ) ) {
+            return;
+        }
+
         // Get the elimin8r logo from the theme directory
         $logo = get_template_directory_uri() . '/dist/images/elimin8r-logo.svg';
 
@@ -39,6 +56,8 @@ class Notices {
             <a href="https://buymeacoffee.com/elimin8r" target="_blank" rel="nofollow noreferrer" style="align-items:center;color:#fff;display:flex;background:linear-gradient(135deg,rgb(20,236,224) 0%,rgb(81,112,254) 50%,rgb(236,13,250) 100%);height:40px;width:140px;justify-content:center;text-decoration:none;border-radius:40px;margin:20px 0;">Buy me a coffee</a>
 
             <p>Stay up-to-date with the latest news and theme updates by visiting <a href="https://www.elimin8r.com" target="_blank" rel="nofollow noreferrer">elimin8r.com</a>.</p>
+
+            <a href="?elimin8r-notice-dismissed">Don't show me again</a>
         </div>
 
         <?php 

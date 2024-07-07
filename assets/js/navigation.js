d9b3b61c1d2c1b509bd5874c748d5174f4a1efa0
Ryan Andrews <rpandrews@live.co.uk>
2024-07-07 07:17:23 +0100

    Changed nav dropdown to function with click only

--- assets/js/navigation.js
+++ assets/js/navigation.js
@@ -67,25 +67,14 @@
 	// Toggle focus each time a menu link with children receive a touch event.
 	for ( const link of linksWithChildren ) {
 		link.addEventListener('touchstart', toggleFocus, false, { passive: true } );
+		link.addEventListener('click', toggleFocus);
 	}
 
 	/**
 	 * Sets or removes .focus class on an element.
 	 */
-	function toggleFocus() {
-		if ( event.type === 'focus' || event.type === 'blur' ) {
-			let self = this;
-			// Move up through the ancestors of the current link until we hit .nav-menu.
-			while ( ! self.classList.contains( 'nav-menu' ) ) {
-				// On li elements toggle the class .focus.
-				if ( 'li' === self.tagName.toLowerCase() ) {
-					self.classList.toggle( 'focus' );
-				}
-				self = self.parentNode;
-			}
-		}
-
-		if ( event.type === 'touchstart' ) {
+	function toggleFocus( event ) {
+		if ( event.type === 'touchstart' || event.type === 'click' ) {
 			const menuItem = this.parentNode;
 			event.preventDefault();
 			for ( const link of menuItem.parentNode.children ) {

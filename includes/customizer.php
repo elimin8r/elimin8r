d4178dcd28e13f083cdf6bc3a4e4f8be45c04993
Ryan Andrews <rpandrews@live.co.uk>
2024-08-17 21:58:11 +0100

    Removed customizer conditional

--- includes/customizer.php
+++ includes/customizer.php
@@ -6,13 +6,12 @@
  * @package elimin8r
  */
 
-if ( ! defined( 'DISABLE_CUSTOMIZER' ) || DISABLE_CUSTOMIZER !== true) {
+if ( ! defined( 'DISABLE_CUSTOMIZER' ) ) {
 	// Include all files in the /customizer directory
 	foreach ( glob( __DIR__ . '/customizer/*.php' ) as $file ) {
 		require_once $file;
 	}
 }
-
 
 class Customizer {
 	public function __construct()

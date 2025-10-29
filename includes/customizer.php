e50ffca4b83e1249c0e560fba23806f91a400631
Ryan Andrews <rpa.andrews@gmail.com>
2025-10-29 15:35:02 +0000

    bugfix: Corrected condition for DISABLE_CUSTOMIZER check in customizer.php

--- includes/customizer.php
+++ includes/customizer.php
@@ -8,7 +8,7 @@ use Elimin8r\Helpers\Helpers;
  * @package elimin8r
  */
 
-if ( ! defined( 'DISABLE_CUSTOMIZER' ) ) {
+if (! defined( 'DISABLE_CUSTOMIZER' ) || ! constant( 'DISABLE_CUSTOMIZER' ) ) {
 	// Include all files in the /customizer directory
 	foreach ( glob( __DIR__ . '/customizer/*.php' ) as $file ) {
 		require_once $file;
